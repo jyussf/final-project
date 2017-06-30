d1a1dabb11bb07800445759dc0583ba1f029ff21
Peter Horton <phorton@softstackfactory.org>
2017-06-30 20:00:38 +0000

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,21 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { LobbyPage } from '../pages/lobby/lobby';
+
+let injections: any[] = [
+    MyApp,
+    LobbyPage
+  ]
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: injections, 
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: injections,
   providers: [
     StatusBar,
     SplashScreen,
